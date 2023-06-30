61ed86af199abe8ebd1f7336b3ae8a721f34d4b4
onoue <k.onoue0724@gmail.com>
2023-06-30 14:08:30 +0900

    finish linalg tests

--- linalg_util.py
+++ linalg_util.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy import linalg
 
 
-def cholesky(matrix, max_tries=5):
+def customized_cholesky(matrix, max_tries=5):
     matrix = np.ascontiguousarray(matrix)
     diag_matrix = np.diag(matrix)
     jitter = diag_matrix.mean() * 1e-6
@@ -27,11 +27,15 @@ def cholesky(matrix, max_tries=5):
     raise linalg.LinAlgError("Matrix is not positive definite, even with jitter.")
 
     
+def generate_pd_matrix(size=None):
+    if size is None:
+        size = 20
+    A = np.random.randn(size, 100)
+    return A @ A.T
+
     
-def generate_non_pd_matrix():    
-    # Create PD matrix
-    A = np.random.randn(20, 100)
-    A = A.dot(A.T)
+def generate_non_pd_matrix(size=None):    
+    A = generate_pd_matrix(size=size)
     # Compute Eigdecomp
     vals, vectors = np.linalg.eig(A)
     # Set smallest eigenval to be negative with 5 rounds worth of jitter
@@ -40,3 +44,16 @@ def generate_non_pd_matrix():
     vals[vals.argmin()] = -default_jitter * (10 ** 3.5)
     A_corrupt = (vectors * vals).dot(vectors.T)
     return A_corrupt
+
+
+def symmetrify_matrix(A, upper=False):
+    triu = np.triu_indices_from(A,k=1)
+    if upper:
+        A.T[triu] = A[triu]
+    else:
+        A[triu] = A.T[triu]
+    return A
+
+
+def test():
+    print("hello")
